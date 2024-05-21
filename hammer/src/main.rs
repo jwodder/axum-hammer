8536c0e4648a208c7f8f6ace4aaa278fe8dcc8b9
John T. Wodder II <git@varonathe.org>
2024-05-21 12:28:41 -0400

    Hammer

--- hammer/src/main.rs
+++ hammer/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+mod buffered_tasks;
+use crate::buffered_tasks::BufferedTasks;
+use clap::Parser;
+use futures_util::TryStreamExt;
+use std::num::NonZeroUsize;
+use std::time::Instant;
+
+#[derive(Clone, Debug, Eq, Parser, PartialEq)]
+struct Arguments {
+    url: url::Url,
+
+    requests: NonZeroUsize,
+
+    workers: NonZeroUsize,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = Arguments::parse();
+    let start = Instant::now();
+    let tasks = BufferedTasks::from_iter(
+        args.workers.get(),
+        std::iter::repeat(args.url)
+            .take(args.requests.get())
+            .map(get_url),
+    );
+    tasks.try_collect::<()>().await?;
+    let elapsed = start.elapsed();
+    println!("{elapsed:?}");
+    Ok(())
+}
+
+async fn get_url(url: url::Url) -> anyhow::Result<()> {
+    reqwest::get(url).await?.error_for_status()?;
+    Ok(())
 }
