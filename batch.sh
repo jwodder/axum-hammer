606bbc6f0b0ca1b12deced327fc318c1610e9689
John T. Wodder II <git@varonathe.org>
2024-05-29 10:53:29 -0400

    batch.sh: Adjustments

--- batch.sh
+++ batch.sh
@@ -3,7 +3,11 @@ set -ex
 
 cd "$(dirname "$0")"
 
-mkdir -p stats
+now="$(date -u +%Y.%m.%d-%H.%M.%SZ)"
+
+mkdir -p stats/"$now"
+
+cargo build -r -p axum-hammer -p axum-nail
 
 cargo run -r -p axum-nail &
 trap "kill $!" EXIT
@@ -12,11 +16,10 @@ while ! nc -w1 -z 127.0.0.1 8080
 do sleep 1
 done
 
-cargo build -r -p axum-hammer
 for endpoint in subpages subpages-arc subpages-service subpages-service-arc
 do
     cargo run -q -r -p axum-hammer -- \
-        subpages "http://127.0.0.1:8080/$endpoint" \
+        subpages --samples 25 "http://127.0.0.1:8080/$endpoint" \
         5 10 15 20 21 22 23 24 25 26 27 28 29 30 40 50 100 \
-        >> stats/"$endpoint.csv"
+        > "stats/$now/$endpoint.csv"
 done
