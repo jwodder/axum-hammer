fec42d9b67037a6762e496f12db9b3dca9957369
John T. Wodder II <git@varonathe.org>
2024-06-04 16:20:05 -0400

    nail: Reduce to one simple endpoint and one simple service

--- batch.sh
+++ batch.sh
@@ -2,8 +2,6 @@
 set -ex
 
 runs=100
-samples=25
-#workers='5 10 15 20 21 22 23 24 25 26 27 28 29 30 40 50 100'
 workers="$(seq -s " " 50)"
 
 cd "$(dirname "$0")"
@@ -21,29 +19,16 @@ do sleep 1
 done
 
 now="$(date -u +%Y.%m.%d-%H.%M.%SZ)"
-mkdir -p stats/"$now"
+mkdir -p stats/one-service-"$now"
 
 while read -r urlpath filename
 do
     cargo run -q -r -p axum-hammer -- run \
-        -o "stats/$now/$filename.json" \
+        -o "stats/one-service-$now/$filename.json" \
         "http://127.0.0.1:8080/$urlpath" \
         "$runs" \
         $workers
 done <<EOT
 hello hello
-sleep?min=10&max=25 sleep-10-25
-foo foo
-foo/bar foo-bar
-foo/custom foo-custom
-simple-service simple-service
+hello-service hello-service
 EOT
-
-for endpoint in subpages subpages-arc subpages-service subpages-service-arc
-do
-    cargo run -q -r -p axum-hammer -- subpages \
-        -o "stats/$now/$endpoint.json" \
-        --samples "$samples" \
-        "http://127.0.0.1:8080/$endpoint" \
-        $workers
-done
--- nail/src/main.rs
+++ nail/src/main.rs
@@ -1,22 +1,8 @@
-mod sleep;
-mod subpages;
-use crate::sleep::{sleep_for_params, sleep_handler, Sleeper};
-use crate::subpages::SubpageService;
 use anyhow::Context;
-use axum::{
-    extract::{Path, Request},
-    routing::get,
-    Router,
-};
+use axum::{extract::Request, routing::get, Router};
 use clap::Parser;
-use rand::thread_rng;
-use std::io::{stderr, IsTerminal};
 use std::net::IpAddr;
-use std::sync::Arc;
 use tower::service_fn;
-use tower_http::trace::TraceLayer;
-use tracing::Level;
-use tracing_subscriber::{filter::Targets, fmt::time::OffsetTime, prelude::*};
 
 #[derive(Clone, Debug, Eq, Parser, PartialEq)]
 struct Arguments {
@@ -27,99 +13,19 @@ struct Arguments {
     /// Port to listen on
     #[arg(short, long, default_value_t = 8080)]
     port: u16,
-
-    #[arg(short = 'T', long)]
-    trace: bool,
-}
-
-// See
-// <https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/time/struct.OffsetTime.html#method.local_rfc_3339>
-// for an explanation of the main + #[tokio::main]run thing
-fn main() -> anyhow::Result<()> {
-    let args = Arguments::parse();
-    if args.trace {
-        let timer =
-            OffsetTime::local_rfc_3339().context("failed to determine local timezone offset")?;
-        tracing_subscriber::registry()
-            .with(
-                tracing_subscriber::fmt::layer()
-                    .with_timer(timer)
-                    .with_ansi(stderr().is_terminal())
-                    .with_writer(stderr),
-            )
-            .with(
-                Targets::new()
-                    .with_target(env!("CARGO_CRATE_NAME"), Level::TRACE)
-                    .with_target("tower_http", Level::TRACE)
-                    .with_default(Level::INFO),
-            )
-            .init();
-    }
-    run(args)
 }
 
 #[tokio::main]
-async fn run(args: Arguments) -> anyhow::Result<()> {
-    let mut rng = thread_rng();
-    let sleeper = Arc::new(Sleeper);
+async fn main() -> anyhow::Result<()> {
+    let args = Arguments::parse();
     let app = Router::new()
         .route("/hello", get(|| async { "Hello, world!\n" }))
-        .route("/sleep", get(sleep_for_params))
-        .route("/foo", get(|| async { "Foo\n" }))
-        .route("/foo/bar", get(|| async { "Foo Bar\n" }))
-        .route(
-            "/foo/:key",
-            get(|Path(key): Path<String>| async move { format!("Foo {key}\n") }),
-        )
-        .nest("/subpages", {
-            let subpages = SubpageService::new("/subpages", &mut rng);
-            let subpages2 = subpages.clone();
-            Router::new()
-                .route("/", get(move || async move { subpages.index_response() }))
-                .route(
-                    "/:key",
-                    get(move |key: Path<String>| async move { subpages2.subpage_response(&key) }),
-                )
-        })
-        .nest("/subpages-arc", {
-            let subpages = Arc::new(SubpageService::new("/subpages-arc", &mut rng));
-            let subpages2 = Arc::clone(&subpages);
-            Router::new()
-                .route("/", get(move || async move { subpages.index_response() }))
-                .route(
-                    "/:key",
-                    get(move |key: Path<String>| async move { subpages2.subpage_response(&key) }),
-                )
-        })
-        .nest_service("/subpages-service", {
-            let subpages = SubpageService::new("/subpages-service", &mut rng);
-            service_fn(move |req: Request| {
-                let s = subpages.clone();
-                async move { s.handle_request(req).await }
-            })
-        })
-        .nest_service("/subpages-service-arc", {
-            let subpages = Arc::new(SubpageService::new("/subpages-service-arc", &mut rng));
-            service_fn(move |req: Request| {
-                let s = Arc::clone(&subpages);
-                async move { s.handle_request(req).await }
-            })
-        })
-        .nest_service("/sleep-service", service_fn(sleep_handler))
         .nest_service(
-            "/sleep-arc-service",
-            service_fn(move |req: Request| {
-                let sleeper = Arc::clone(&sleeper);
-                async move { sleeper.handle(req).await }
+            "/hello-service",
+            service_fn(|_: Request| async move {
+                Ok::<_, std::convert::Infallible>("Hello from a service!\n")
             }),
-        )
-        .nest_service(
-            "/simple-service",
-            service_fn(|req: Request| async move {
-                Ok::<_, std::convert::Infallible>(req.uri().path().to_owned())
-            }),
-        )
-        .layer(TraceLayer::new_for_http());
+        );
     let listener = tokio::net::TcpListener::bind((args.ip_addr, args.port))
         .await
         .context("failed to bind listener")?;
